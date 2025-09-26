0f365afd02429724b8f9b1d2728c25e2f711dfd9
MattMarti <matthew.marti@gmail.com>
2025-09-25 22:56:41 -0400

    Draw a simple stationary plot in minifb window

--- rust/plotters_normal_graph/src/main.rs
+++ rust/plotters_normal_graph/src/main.rs
@@ -4,88 +4,123 @@ use std::collections::VecDeque;
 use std::error::Error;
 use std::time::SystemTime;
 use std::borrow::{Borrow, BorrowMut};
+use std::thread::sleep;
+use std::time::Duration;
 
 // Minifb tools
-use minifb::{Key, KeyRepeat, Window, WindowOptions};
+use plotters_bitmap::bitmap_pixel::BGRXPixel;
+use plotters_bitmap::BitMapBackend;
+use minifb::{Key, Window, WindowOptions};
 
 use plotters::prelude::*;
 
+struct BufferWrapper(Vec<u32>);
+impl Borrow<[u8]> for BufferWrapper {
+    fn borrow(&self) -> &[u8] {
+        unsafe {
+            std::slice::from_raw_parts(
+                self.0.as_ptr() as *const u8,
+                self.0.len() * 4
+            )
+        }
+    }
+}
+impl BorrowMut<[u8]> for BufferWrapper {
+    fn borrow_mut(&mut self) -> &mut [u8] {
+        unsafe {
+            std::slice::from_raw_parts_mut(
+                self.0.as_mut_ptr() as *mut u8,
+                self.0.len() * 4
+            )
+        }
+    }
+}
+impl Borrow<[u32]> for BufferWrapper {
+    fn borrow(&self) -> &[u32] {
+        self.0.as_slice()
+    }
+}
+impl BorrowMut<[u32]> for BufferWrapper {
+    fn borrow_mut(&mut self) -> &mut [u32] {
+        self.0.as_mut_slice()
+    }
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    const OUT_FILE_NAME: &str = "sample_plot.png";
-    let root_area = BitMapBackend::new(OUT_FILE_NAME, (1024, 768)).into_drawing_area();
+    const WIDTH: usize = 800;
+    const HEIGHT: usize = 600;
 
-    root_area.fill(&WHITE);
+    let mut buf = BufferWrapper(vec![0u32; WIDTH * HEIGHT]);
 
-    let root_area = root_area.titled("Image Title", ("sans-serif", 60))?;
+    {
+        let root_area = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
+            buf.borrow_mut(), (WIDTH as u32, HEIGHT as u32)
+        )
+        .unwrap()
+        .into_drawing_area();
 
-    let (upper, lower) = root_area.split_vertically(512);
+        root_area.fill(&WHITE);
 
-    let x_axis = (-3.4f32..3.4).step(0.1);
+        let root_area = root_area.titled("Image Title", ("sans-serif", 60))?;
 
-    let mut cc = ChartBuilder::on(&upper)
-        .margin(5)
-        .set_all_label_area_size(50)
-        .caption("Sine and Cosine", ("sans-serif", 40))
-        .build_cartesian_2d(-3.4f32..3.4, -1.2f32..1.2f32)?;
+        let x_axis = (-3.4f32..3.4).step(0.1);
 
-    cc.configure_mesh()
-        .x_labels(20)
-        .y_labels(10)
-        .disable_mesh()
-        .x_label_formatter(&|v| format!("{:.1}", v))
-        .y_label_formatter(&|v| format!("{:.1}", v))
-        .draw()?;
+        let mut cc = ChartBuilder::on(&root_area)
+            .margin(5)
+            .set_all_label_area_size(50)
+            .caption("Sine and Cosine", ("sans-serif", 40))
+            .build_cartesian_2d(-3.4f32..3.4, -1.2f32..1.2f32)?;
 
-    cc.draw_series(LineSeries::new(x_axis.values().map(|x| (x, x.sin())), &RED))?
-        .label("Sine")
-        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
-
-    cc.draw_series(LineSeries::new(
-        x_axis.values().map(|x| (x, x.cos())),
-        &BLUE,
-    ))?
-    .label("Cosine")
-    .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLUE));
-
-    cc.configure_series_labels().border_style(BLACK).draw()?;
-
-    cc.draw_series(PointSeries::of_element(
-        (-3.0f32..2.1f32).step(1.0).values().map(|x| (x, x.sin())),
-        5,
-        ShapeStyle::from(&RED).filled(),
-        &|coord, size, style| {
-            EmptyElement::at(coord)
-                + Circle::new((0, 0), size, style)
-                + Text::new(format!("{:?}", coord), (0, 15), ("sans-serif", 15))
-        },
-    ))?;
-
-    let drawing_areas = lower.split_evenly((1, 2));
-
-    for (drawing_area, idx) in drawing_areas.iter().zip(1..) {
-        let mut cc = ChartBuilder::on(drawing_area)
-            .x_label_area_size(30)
-            .y_label_area_size(30)
-            .margin_right(20)
-            .caption(format!("y = x^{}", 1 + 2 * idx), ("sans-serif", 40))
-            .build_cartesian_2d(-1f32..1f32, -1f32..1f32)?;
         cc.configure_mesh()
-            .x_labels(5)
-            .y_labels(3)
-            .max_light_lines(4)
+            .x_labels(20)
+            .y_labels(10)
+            .disable_mesh()
+            .x_label_formatter(&|v| format!("{:.1}", v))
+            .y_label_formatter(&|v| format!("{:.1}", v))
             .draw()?;
 
+        cc.draw_series(LineSeries::new(x_axis.values().map(|x| (x, x.sin())), &RED))?
+            .label("Sine")
+            .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], RED));
+
         cc.draw_series(LineSeries::new(
-            (-1f32..1f32)
-                .step(0.01)
-                .values()
-                .map(|x| (x, x.powf(idx as f32 * 2.0 + 1.0))),
+            x_axis.values().map(|x| (x, x.cos())),
             &BLUE,
+        ))?
+        .label("Cosine")
+        .legend(|(x, y)| PathElement::new(vec![(x, y), (x + 20, y)], BLUE));
+
+        cc.configure_series_labels().border_style(BLACK).draw()?;
+
+        cc.draw_series(PointSeries::of_element(
+            (-3.0f32..2.1f32).step(1.0).values().map(|x| (x, x.sin())),
+            5,
+            ShapeStyle::from(&RED).filled(),
+            &|coord, size, style| {
+                EmptyElement::at(coord)
+                    + Circle::new((0, 0), size, style)
+                    + Text::new(format!("{:?}", coord), (0, 15), ("sans-serif", 15))
+            },
         ))?;
     }
+    let mut window = Window::new(
+        "My Plot",
+        WIDTH,
+        HEIGHT,
+        WindowOptions::default()
+    ).unwrap();
 
-    root_area.present().expect("Unalbe to write file");
-    println!("Saved plot");
+    //let root = BitMapBackend::<BGRXPixel>::with_buffer_and_format(
+    //    buf.borrow_mut(),
+    //    (WIDTH as u32, HEIGHT as u32),
+    //)
+    //.unwrap()
+    //.into_drawing_area();
+
+    while window.is_open() && !window.is_key_down(Key::Escape) {
+        window.update_with_buffer(buf.borrow(), WIDTH, HEIGHT).unwrap();
+        sleep(Duration::from_millis(50));
+    }
+
     Ok(())
 }
